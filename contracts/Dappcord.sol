67d6a1fd9c82078afb6e3053d7720664cdf51c29
krishh16 <krishh16@yahoo.com>
2024-01-18 13:47:50 +0530

    added smart contract, test and deployment script

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -4,33 +4,56 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Dappcord is ERC721 {
-  address public owner;
-  uint256 public totalChannels;
-  struct Channel {
-    uint256 id;
-    string name;
-    uint256 cost;
-  }
-  modifier onlyOwner{
-    require(msg.sender == owner, 'Only owner can run this function');
-    _;
-  }
-  mapping(uint256 => Channel) public channels;
-  constructor(string memory _name, string memory _symbol) 
-  ERC721(_name, _symbol)
-  {
-  // assigns the deployer of the contract as the owner
-  owner = msg.sender;  
-    
-  }
+    uint256 public totalSupply;
+    uint256 public totalChannels;
+    address public owner;
 
-  function createChannel(string memory _name, uint256 _cost) public onlyOwner{
-    totalChannels++;
+    struct Channel {
+        uint256 id;
+        string name;
+        uint256 cost;
+    }
 
-    channels[totalChannels] = Channel(totalChannels, _name, _cost);   
-    
-  }
-  function getChannel(uint256 _id) public view returns( Channel memory) {
-    return channels[_id];
-  }
+    mapping(uint256 => Channel) public channels;
+    mapping(uint256 => mapping(address => bool)) public hasJoined;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {
+        owner = msg.sender;
+    }
+
+    function createChannel(string memory _name, uint256 _cost)
+        public
+        onlyOwner
+    {
+        totalChannels++;
+        channels[totalChannels] = Channel(totalChannels, _name, _cost);
+    }
+
+    function mint(uint256 _id) public payable {
+        require(_id != 0);
+        require(_id <= totalChannels);
+        require(hasJoined[_id][msg.sender] == false);
+        require(msg.value >= channels[_id].cost);
+
+        hasJoined[_id][msg.sender] = true;
+        totalSupply++;
+
+        _safeMint(msg.sender, totalSupply);
+    }
+
+    function getChannel(uint256 _id) public view returns (Channel memory) {
+        return channels[_id];
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,8 +5,29 @@ const tokens = (n) => {
 }
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  const NAME  = "Dappcord"
+  const SYMBOL = "DC"
 
-}
+  const Dappcord = await ethers.getContractFactory("Dappcord")
+
+  const dappcord = await Dappcord.deploy(NAME, SYMBOL)
+  await dappcord.deployed();
+
+  console.log(`Deployed Dappcord Contract at: ${dappcord.address}`)
+
+
+  const CHANNEL_NAMES  =  ['general', 'intro', 'jobs']
+  const COSTS = [tokens(1), tokens(0), tokens(0.25)]
+  for (var i = 0; i < 3; i++){
+    const transaction = await dappcord.connect(deployer).createChannel(CHANNEL_NAMES[i], COSTS[i])
+
+    await transaction.wait();
+
+    console.log(`Created text channel ${CHANNEL_NAMES[i]}`
+    )
+  }
+} 
 
 main().catch((error) => {
   console.error(error);
